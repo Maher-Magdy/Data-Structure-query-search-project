4b9243af180c7171fef47ec271e29eaa4a6134b1
Maher Magdy <61353846+Maher-Magdy@users.noreply.github.com>
2020-05-29 00:14:43 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Data_Index_test0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadATreeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usePreviousIndexToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(130, 255);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Gray;
+            this.textBox1.Location = new System.Drawing.Point(130, 195);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Tag = "Enter the word";
+            this.textBox1.Text = "Enter the word";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(130, 128);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter the word";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(415, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.loadATreeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.fileToolStripMenuItem.Text = "Open";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.openToolStripMenuItem.Text = "New";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // loadATreeToolStripMenuItem
+            // 
+            this.loadATreeToolStripMenuItem.Name = "loadATreeToolStripMenuItem";
+            this.loadATreeToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.loadATreeToolStripMenuItem.Text = "Load";
+            this.loadATreeToolStripMenuItem.Click += new System.EventHandler(this.loadATreeToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usePreviousIndexToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // usePreviousIndexToolStripMenuItem
+            // 
+            this.usePreviousIndexToolStripMenuItem.Checked = true;
+            this.usePreviousIndexToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.usePreviousIndexToolStripMenuItem.Name = "usePreviousIndexToolStripMenuItem";
+            this.usePreviousIndexToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.usePreviousIndexToolStripMenuItem.Text = "Use previous Index";
+            this.usePreviousIndexToolStripMenuItem.Click += new System.EventHandler(this.usePreviousIndexToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(101, 354);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "No.";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label3.Location = new System.Drawing.Point(278, 438);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Next";
+            this.label3.Visible = false;
+            this.label3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label3_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(415, 543);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Query Search";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripMenuItem loadATreeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usePreviousIndexToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+using System.Collections;
+using System.Text.RegularExpressions;
+namespace Data_Index_test0
+{
+    public partial class Form1 : Form
+    {
+
+        //public
+        string location="" ;
+        string treeLocation = "";
+        string cPath="data index c++.exe";
+        bool first = true;
+        int noOfResultFiles = 0;
+        int command = 0;
+        void TextWrite(string name, string data)
+        {
+            string path = @name + ".txt";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            StreamWriter text = new StreamWriter(path);
+            text.Write("{0}\r\n", data);
+            text.Close();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((location == "")) { MessageBox.Show("Choose a file location first"); }
+
+            else
+
+            {
+                TextWrite("The Path", location);//for inverse use newLocationString
+
+                TextWrite("The Word", command.ToString());
+                TextWrite("The Result", "not found");
+                TextWrite("Done", "0");
+                TextWrite("Read", "1");
+               
+               
+                //call c++ only one time
+               if (first == true) { Process.Start(cPath); first = false; }
+
+                //wait for done
+                while (true)
+
+                {
+                    button1.Text = "Searching...";
+                    button1.Enabled = false;
+                    Thread.Sleep(230);
+
+                    try
+                    {
+                        string[] done = File.ReadAllLines("Done.txt");
+                        if (done[0] == "1")
+
+                        {
+                            button1.Text = "Search";
+                            button1.Enabled = true;
+                            TextWrite("Done", "0");
+                            break;
+
+
+
+                        }
+                    }
+
+                    catch
+                    {
+                        Thread.Sleep(25);
+                        string[] done = File.ReadAllLines("Done.txt");
+                        if (done[0] == "1")
+
+                        {
+                            button1.Text = "Search";
+                            button1.Enabled = true;
+                            TextWrite("Done", "0");
+                            break;
+
+
+
+                        }
+                    }
+
+
+                    
+
+                }
+
+                //command 4
+                if (command == 1)
+                {
+                    command = 4;
+                    TextWrite("The Word", command.ToString());
+                    TextWrite("Read", "1");
+                
+                    //wait for done
+                while (true)
+
+                {
+                        button1.Text = "Searching...";
+                        button1.Enabled = false;
+                        try
+                    {
+                        string[] done = File.ReadAllLines("Done.txt");
+                        if (done[0] == "1")
+
+                        {
+                                button1.Text = "Search";
+                                button1.Enabled = true;
+                                TextWrite("Done","0");
+                                break;
+
+
+
+                        }
+                    }
+
+                    catch
+                        {
+                            Thread.Sleep(25);
+                            string[] done = File.ReadAllLines("Done.txt");
+                            if (done[0] == "1")
+
+                            {
+                                button1.Text = "Search";
+                                button1.Enabled = true;
+                                TextWrite("Done", "0");
+                                break;
+
+
+
+                            }
+                        }
+
+
+                    Thread.Sleep(230);
+
+                }
+            }
+                if (command==2||command==3||command==4) 
+                {
+                    command = 3;
+                    TextWrite("The Word", command.ToString()+textBox1.Text);
+                    TextWrite("Read", "1");
+                }
+                //wait for done
+                while (true)
+
+                {
+                    button1.Text = "Searching...";
+                    button1.Enabled = false;
+                    try
+                    {
+                        string[] done = File.ReadAllLines("Done.txt");
+                        if (done[0] == "1")
+
+                        {
+                            button1.Text = "Search";
+                            button1.Enabled = true;
+                            TextWrite("Done", "0");
+                            break;
+
+
+
+                        }
+                    }
+
+                    catch
+                    {
+                        Thread.Sleep(25);
+                        string[] done = File.ReadAllLines("Done.txt");
+                        if (done[0] == "1")
+
+                        {
+                            button1.Text = "Search";
+                            button1.Enabled = true;
+                            TextWrite("Done", "0");
+                            break;
+
+
+
+                        }
+                    }
+
+
+                    Thread.Sleep(230);
+
+                }
+                //Thread.Sleep(500);
+                //show results
+                string[] resultFiles = File.ReadAllLines("The Result.txt");
+                
+                //open five files
+                if (resultFiles[0] != "not found")
+                {
+                    label3.Visible = false;
+                    label2.Visible = true;
+                    label2.Text = resultFiles.Length.ToString() + " Files Contain " + "\"" + textBox1.Text + "\""  +  " !";
+                    for (noOfResultFiles = 0; noOfResultFiles < 5; noOfResultFiles++)
+
+                    {
+                        if(noOfResultFiles >= resultFiles.Length) { label3.Visible = false; break; }
+                        Process.Start(resultFiles[noOfResultFiles]);
+                       
+                    }
+                }
+                //no result
+                else { label2.Visible = true; label2.Text = "No Files Contain " + "\"" + textBox1.Text + "\"" + " !"; label3.Visible = false; }
+                //next
+                if (resultFiles.Length>5) { label3.Visible=true; }
+
+
+            }
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+      
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label3.Visible = false;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            FolderBrowserDialog file = new FolderBrowserDialog();
+            label2.Visible = false;
+            label3.Visible = false;
+            if (file.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+            {
+
+                 location = file.SelectedPath+"\\";
+                command = 1;
+                TextWrite("The Word", "0");
+                TextWrite("Read", "1");
+                first = true;
+
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TextWrite("The Word","0");
+            TextWrite("Read", "1");
+            this.Close();
+           
+        }
+
+        private void label3_MouseClick(object sender, MouseEventArgs e)
+        {
+            //show next 5 text files
+            
+            string[] resultFiles = File.ReadAllLines("The Result.txt");
+
+
+            int i = 0;
+                while(true)
+
+                {
+              
+                if (i==5) { break; }
+                if (noOfResultFiles >= resultFiles.Length) { label3.Visible = false;break; }
+                 Process.Start(resultFiles[noOfResultFiles]); noOfResultFiles++; i++;
+            }
+            
+            
+
+
+        }
+
+        private void loadATreeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            //if (location=="") { MessageBox.Show("Choose a file location first"); }
+            label2.Visible = false;
+            label3.Visible = false;
+          
+            FolderBrowserDialog file = new FolderBrowserDialog();
+
+            if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // TextWrite("The Inverted Index", file.FileName );
+                location = file.SelectedPath + "\\";
+                command = 2;
+                TextWrite("The Word", "0");
+                TextWrite("Read", "1");
+                first = true;
+
+            }
+        }
+
+        private void usePreviousIndexToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (usePreviousIndexToolStripMenuItem.Checked==true) { usePreviousIndexToolStripMenuItem.Checked = false; }
+            else if (usePreviousIndexToolStripMenuItem.Checked == false) { usePreviousIndexToolStripMenuItem.Checked = true; }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+/* Process.Start(cPath);
+                while (true)
+                
+                {
+                    string[] resultFiles = File.ReadAllLines("The Result.txt");
+                    if (resultFiles[0] != "")
+                    
+                    {
+                        //open all files
+                        for (int i = 0; i < resultFiles.Length; i++)
+
+                        {
+                            Process.Start(resultFiles[i]);
+                            
+                        }
+
+                        label2.Text ="Number of Files Containing "+"\""+textBox1.Text+"\""+" is "+resultFiles.Length.ToString()+" !";
+                        break;
+                    
+                    }
+                    Thread.Sleep(250);
+                
+                }*/
+
+/*   //invert slash
+            char[] newLocation = new char[location.Length];
+            string newLocationString = "";
+            for (int i=0;i<location.Length;i++)
+            {
+                if (location[i] == '\\')
+                { newLocation[i] = '/'; }
+                else
+                { newLocation[i] = location[i]; }
+            }
+
+
+            for (int i = 0; i < location.Length; i++) { newLocationString += newLocation[i]; }
+            */
